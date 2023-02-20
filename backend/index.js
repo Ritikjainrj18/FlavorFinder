2257a1e6c7b1426a1fa71e950314d01c3eb18777
Ritik <ritikjainrj18@gmail.com>
2023-02-20 15:46:38 +0530

    Revert "m"
    This reverts commit 6c543564acaea7b4aec915d7833e7262c312a0b7.

--- backend/index.js
+++ backend/index.js
@@ -6,19 +6,22 @@ const MongoClient = mongodb.MongoClient
 const port= process.env.PORT || 8000
 import RestaurantsDAO from "./dao/restaurantsDAO.js"
 import ReviewsDAO from "./dao/reviewsDAO.js"
-let URL="mongodb+srv://ritikjainrj18:6232547429@cluster0.xrzcv7u.mongodb.net/sample_restaurants?retryWrites=true&w=majority"
 MongoClient.connect(
-    URL
-    // process.env.RESTREVIEWS_DB_URI,
+    process.env.RESTREVIEWS_DB_URI,
+    {
+        // poolSize: 50,
+        // wtimeoutMS:2500,
+        // useNewUrlParse: true  
+    }
 )
+.catch(err=>{
+    console.error(err.stack)
+    process.exit(1)
+})
 .then(async client=>{
     await RestaurantsDAO.injectDB(client)
     await ReviewsDAO.injectDB(client)
     app.listen(port,()=>{
         console.log(`listening on port ${port}`)
     })
-})
-.catch(err=>{
-    console.error(err.stack)
-    process.exit(1)
 })
